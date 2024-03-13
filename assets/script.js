e9e082f78410fc6ef77212ef463c3c14f6a0b922
VitorJs00 <161177573+VitorJs00@users.noreply.github.com>
2024-03-12 23:51:57 -0300

    for iarea

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var nn = document.getElementById('nn')
+function nao(){
+    nn.innerHTML='SIM'
+    nn.style.background='green'
+}
+function nao2(){
+    nn.innerHTML='NAO???'
+    nn.style.background='orange'
+    alert('BESTAAAAAAA')
+    alert('BESTAAAAAAA')
+    alert('BESTAAAAAAA')
+}
